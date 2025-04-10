e8244d49387fddfa3bdf5be2837d5df27f5753e8
LKenji <lucaskamikawa@gmail.com>
2025-04-10 00:30:29 -0300

    inicio base

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+#python manage.py runserver  
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)  # Nome da tag (único)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag, blank=True)  # Relação Many-to-Many com Tag
+    favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}. {self.title}"
