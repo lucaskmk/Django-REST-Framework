03cc58528d9e3b9950f2b8ff44055679c337dc77
LKenji <lucaskamikawa@gmail.com>
2025-04-10 00:52:46 -0300

    fim

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+@api_view(['GET', 'PUT', 'DELETE'])
+def api_note(request, note_id):
+    try:
+        note = Note.objects.get(id=note_id)
+    except Note.DoesNotExist:
+        raise Http404()
+
+    if request.method == 'PUT':
+        new_note_data = request.data
+        note.title = new_note_data['title']
+        note.content = new_note_data['content']
+        note.save()
+
+    if request.method == 'Delete':
+        note.delete()
+
+    serialized_note = NoteSerializer(note)
+    return Response(serialized_note.data)
+
+@api_view(['GET', 'POST'])
+def api_notes(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            notes = Note.objects.all()
+            serializer_all = NoteSerializer(notes, many=True)
+            return Response(serializer_all.data)
+        return Response(serializer.errors, status=400)
